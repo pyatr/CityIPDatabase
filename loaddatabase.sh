5761fc0d51249749c169d6a0e7facbf6c0f8d328
pyatr <44798005+pyatr@users.noreply.github.com>
2024-08-10 19:18:01 +0700

    Compile project on docker up
    Also optimization for WSL

--- loaddatabase.sh
+++ loaddatabase.sh
@@ -1,7 +1,6 @@
 service mysql start
 unrar e dump.rar
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS ipcitydatabase"
-mysql -u root -e "CREATE USER 'ipcityservice'@'%' IDENTIFIED BY '1234';"
-mysql -u root ipcitydatabase < dump3.sql
-mysql -u root -e "GRANT ALL ON ipcitydatabase.iprangelocation TO 'ipcityservice'@'%'";
-mysql -u root -e "FLUSH PRIVILEGES";
+mysql -u root -e "CREATE USER 'ipcityservice'@'%' IDENTIFIED BY '1234'"
+mysql -u root < dump3.sql
+mysql -u root -e "GRANT ALL ON ipcitydatabase.iprangelocation TO 'ipcityservice'@'%'"
+mysql -u root -e "FLUSH PRIVILEGES"
--- startdb.sh
+++ startdb.sh
@@ -1,7 +1,7 @@
 service mysql start
 service apache2 start
-# cd /var/www/html/get-city-by-ip
-# composer install
-# npm run build
+cd /var/www/html/get-city-by-ip && composer install
+cd /var/www/html/get-city-by-ip && npm install
+cd /var/www/html/get-city-by-ip && npm run build
 #Infinite waiting because tty: true is just not good enough for docker to keep container running
 sh -c tail -f /dev/null
