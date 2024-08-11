61373edeb2f73cde93be608d4d7402559388968d
pyatr <44798005+pyatr@users.noreply.github.com>
2024-08-11 19:01:21 +0700

    Add key generate command to start

--- startdb.sh
+++ startdb.sh
@@ -3,5 +3,6 @@ service apache2 start
 cd /var/www/html/get-city-by-ip && composer install
 cd /var/www/html/get-city-by-ip && npm install
 cd /var/www/html/get-city-by-ip && npm run build
+cd /var/www/html/get-city-by-ip && php bin/console lexik:jwt:generate-keypair
 #Infinite waiting because tty: true is just not good enough for docker to keep container running
 sh -c tail -f /dev/null
